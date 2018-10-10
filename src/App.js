3ba8dc452abeb523c9f751d42490132d2331b5f2
Jason Petterson <jp@brokenshotgun.com>
2018-10-10 14:02:08 -0400

    started parsing example response, refactored data structure
    Also added a scratch pad to help with keeping track of TODOs and various
    Calendar API stuff.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
+import gcalEventsData from './events.json'
 import './App.css';
 //import ReactDOM from 'react-dom';
 
@@ -7,85 +8,102 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      venueData: [
-        {
-          id: 0,
-          venueName: "Planet Ant Black Box",
-          venueAddress: "2357 Caniff St, Hamtramck, MI 48212",
+      venueData: {
+        "Planet Ant Black Box" : {
+          address: "2357 Caniff St, Hamtramck, MI 48212",
           eventsData: {
-            "1am" : {
-              id: 0,
-              eventStartTime: "00:00",
-              eventEndTime: "01:00",
-              eventName: "Test",
-              venueId: 0
+            "1am": {
+              eventName: "Foo Test",
             }
           }
         },
-        {
-          id: 1,
-          venueName: "Planet Ant Black Box (2nd Floor)",
-          venueAddress: "2357 Caniff St, Hamtramck, MI 48212",
-          eventsData: { }
+        "Planet Ant Black Box (2nd Floor)" : {
+          address: "2357 Caniff St, Hamtramck, MI 48212",
+          eventsData: {
+            "7pm": {
+              eventName: "Bar Test",
+            }
+          }
         },
-        {
-          id: 2,
-          venueName: "Ant Hall (Main Stage)",
-          venueAddress: "2320 Caniff St, Hamtramck, MI 48212",
-          eventsData: { }
+        "Ant Hall (Main Stage)" : {
+          address: "2320 Caniff St, Hamtramck, MI 48212",
+          eventsData: {}
         },
-        {
-          id: 3,
-          venueName: "Ant Hall (Green Room)",
-          venueAddress: "2320 Caniff St, Hamtramck, MI 48212",
-          eventsData: { }
+        "Ant Hall (Green Room)" : {
+          address: "2320 Caniff St, Hamtramck, MI 48212",
+          eventsData: {}
         },
-        {
-          id: 4,
-          venueName: "Ant Hall (New Bros)",
-          venueAddress: "2320 Caniff St, Hamtramck, MI 48212",
-          eventsData: { }
+        "Ant Hall (New Bros)" : {
+          address: "2320 Caniff St, Hamtramck, MI 48212",
+          eventsData: {}
         },
-        {
-          id: 5,
-          venueName: "Ant Hall (Front Room)",
-          venueAddress: "2320 Caniff St, Hamtramck, MI 48212",
-          eventsData: { }
+        "Ant Hall (Front Room)" : {
+          address: "2320 Caniff St, Hamtramck, MI 48212",
+          eventsData: {}
         },
-        {
-          id: 6,
-          venueName: "Podcast Studio",
-          venueAddress: "11831 Joseph Campau Ave, Hamtramck, MI 48212",
-          eventsData: { }
+        "Podcast Studio" : {
+          address: "11831 Joseph Campau Ave, Hamtramck, MI 48212",
+          eventsData: {
+            "5pm": {
+              eventName: "Pod Test",
+            }
+          }
         }
-      ],
+      },
       timeData: [
-        { rowLabel: "1am" },
-        { rowLabel: "2am" },
-        { rowLabel: "3am" },
-        { rowLabel: "4am" },
-        { rowLabel: "5am" },
-        { rowLabel: "6am" },
-        { rowLabel: "7am" },
-        { rowLabel: "8am" },
-        { rowLabel: "9am" },
-        { rowLabel: "10am" },
-        { rowLabel: "11am" },
-        { rowLabel: "12am" },
-        { rowLabel: "1pm" },
-        { rowLabel: "2pm" },
-        { rowLabel: "3pm" },
-        { rowLabel: "4pm" },
-        { rowLabel: "5pm" },
-        { rowLabel: "6pm" },
-        { rowLabel: "7pm" },
-        { rowLabel: "8pm" },
-        { rowLabel: "9pm" },
-        { rowLabel: "10pm" },
-        { rowLabel: "11pm" },
-        { rowLabel: "12pm" }
+        "1am",
+        "2am",
+        "3am",
+        "4am",
+        "5am",
+        "6am",
+        "7am",
+        "8am",
+        "9am",
+        "10am",
+        "11am",
+        "12am",
+        "1pm",
+        "2pm",
+        "3pm",
+        "4pm",
+        "5pm",
+        "6pm",
+        "7pm",
+        "8pm",
+        "9pm",
+        "10pm",
+        "11pm",
+        "12pm"
       ]
     }
+  }
+
+  componentDidMount() {
+    getCalendarEvents().then(response => {
+      //console.log(response)
+      this.parseCalendarEventsResponse(response)
+    });
+  }
+
+  parseCalendarEventsResponse(response) {
+    // TODO clear old eventsData
+    //this.state.venueData.eventsData = {}
+    //this.setState({venueData})
+
+    response.items.map(event => {
+      console.log(event)
+      if (event.location) {
+        //const venue = event.location
+        //const eventName = event.summary
+        if (event.start && event.end) {
+          //const startTime = event.start.dateTime
+          //const endTime = event.end.dateTime
+          //newEvent.eventName = eventName
+        } 
+      }     
+      return 1
+    });
   }
 
   render() {
@@ -97,23 +115,29 @@ class App extends Component {
   }
 }
 
+async function getCalendarEvents() {
+  return gcalEventsData
+}
+
 class VenueTable extends React.Component {
   render() {
-    let columns = this.props.venueData.map(venue => {
-      return <VenueCell key={venue.id} venue={venue} />
-    })
+    const venues = Object.keys(this.props.venueData)
+    let columns = venues.map(venueName => {
+      return <VenueCell key={venueName} venueName={venueName} />
+    });
+    console.log(columns)
 
     const rows = [];
     for (let i = 0; i < this.props.timeData.length; i++) {
       let children = []
-      let rowLabel = this.props.timeData[i].rowLabel
+      let rowLabel = this.props.timeData[i]
       children.push(<TimeCell key={i} time={this.props.timeData[i]} />) // time
-      for (let j = 0; j < this.props.venueData.length; j++) {
-        if (this.props.venueData[j].eventsData.hasOwnProperty(rowLabel)) {
-          children.push(<EventCell key={rowLabel+j} event={this.props.venueData[j].eventsData[rowLabel]} />) // event data
+      for (let j = 0; j < venues.length; j++) {
+        if (this.props.venueData[venues[j]].eventsData.hasOwnProperty(rowLabel)) {
+          children.push(<EventCell key={rowLabel + j} event={this.props.venueData[venues[j]].eventsData[rowLabel]} />) // event data
         }
         else {
-          children.push(<EventCell key={rowLabel+j} event={null} />)
+          children.push(<EventCell key={rowLabel + j} event={null} />)
         }
       }
       rows.push(<TimeRow key={i} children={children} />)
@@ -122,7 +146,7 @@ class VenueTable extends React.Component {
     return (
       <table width="100%">
         <thead>
-          <tr><th/>{columns}</tr>
+          <tr><th />{columns}</tr>
         </thead>
         <tbody>{rows}</tbody>
       </table>
@@ -140,9 +164,9 @@ class TimeRow extends React.Component {
 
 class VenueCell extends React.Component {
   render() {
-    const venue = this.props.venue;
+    const venueName = this.props.venueName
     return (
-      <th>{venue.venueName}</th>
+      <th>{venueName}</th>
     );
   }
 }
@@ -151,7 +175,7 @@ class TimeCell extends React.Component {
   render() {
     const time = this.props.time;
     return (
-      <td>{time.rowLabel}</td>
+      <td>{time}</td>
     );
   }
 }
@@ -165,7 +189,7 @@ class EventCell extends React.Component {
       );
     }
 
-    return (<td/>)
+    return (<td />)
   }
 }
 
