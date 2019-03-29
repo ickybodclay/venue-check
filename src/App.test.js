776c66230bb0f3a16edf8d7f658aa4f63e8cc701
Omar Hyjek <ohyjek@gmail.com>
2019-03-29 15:03:35 -0400

    burn it down

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { mount } from "enzyme";
-import App from "./App";
-
-it("renders without crashing", () => {
-  const wrapper = mount(<App />);
-  const appInstance = wrapper.instance();
-  expect(appInstance).toBeDefined();
-});
