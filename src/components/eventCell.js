2e143aa71acddabdb8300d6f42d02683dfd73fd2
Omar Hyjek <ohyjek@gmail.com>
2019-03-29 16:12:12 -0400

    refactor eventCell to SFC

--- src/components/eventCell.js
+++ src/components/eventCell.js
@@ -1,12 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class EventCell extends Component {
-  render() {
-    const event = this.props.event;
-    if (event) {
-      return <td className="busy-cell">{event.name}</td>;
-    }
+// export class EventCell extends Component {
+//   render() {
+//     const event = this.props.event;
+//     if (event) {
+//       return <td className="busy-cell">{event.name}</td>;
+//     }
 
+//     return <td className="event" />;
+//   }
+// }
+
+export function EventCell(props) {
+  const { event } = props;
+  if (event) {
+    return <td className="busy-cell">{event.name}</td>;
+  } else {
     return <td className="event" />;
   }
 }
