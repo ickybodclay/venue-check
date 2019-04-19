a24d1402764e6f0c8018309a95c90b301358ff73
Jason Petterson <jp@brokenshotgun.com>
2019-04-19 12:03:16 -0400

    added error boundary

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import React from "react";
 
 //components
 import { App } from "./components/app";
+import { ErrorBoundary } from "./components/errorBoundary";
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(<ErrorBoundary><App /></ErrorBoundary>, mountNode);
